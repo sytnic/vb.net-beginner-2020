993daef38ebe3d2219108e208e6a84dbda7bfa93
sytnic <sytnic@list.ru>
2025-04-08 09:21:35 +0300

    5.0 Input with Windows Forms. Input box

---
+++ WindowsApp5/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        Dim stFirstName As String
+
+        stFirstName = InputBox("please enter your first name")
+
+        MsgBox("hello " & stFirstName)
+    End Sub
+End Class
