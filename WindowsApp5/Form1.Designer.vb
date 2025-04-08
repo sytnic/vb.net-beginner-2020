974cdf7b1743961cf6e6729d12ee3945e4a5163a
sytnic <sytnic@list.ru>
2025-04-08 09:41:30 +0300

    5.1 Input with Windows Forms. Text box

--- WindowsApp5/Form1.Designer.vb
+++ WindowsApp5/Form1.Designer.vb
@@ -23,6 +23,12 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.btnGo = New System.Windows.Forms.Button()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.txtGender = New System.Windows.Forms.TextBox()
+        Me.lblGender = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'btnGo
@@ -34,17 +40,78 @@ Partial Class Form1
         Me.btnGo.Text = "Go"
         Me.btnGo.UseVisualStyleBackColor = True
         '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(341, 89)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(147, 20)
+        Me.txtFirstName.TabIndex = 1
+        '
+        'lblFirstName
+        '
+        Me.lblFirstName.AutoSize = True
+        Me.lblFirstName.Location = New System.Drawing.Point(238, 93)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(57, 13)
+        Me.lblFirstName.TabIndex = 2
+        Me.lblFirstName.Text = "First Name"
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(341, 143)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(147, 20)
+        Me.txtLastName.TabIndex = 3
+        '
+        'lblLastName
+        '
+        Me.lblLastName.AutoSize = True
+        Me.lblLastName.Location = New System.Drawing.Point(238, 146)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(58, 13)
+        Me.lblLastName.TabIndex = 4
+        Me.lblLastName.Text = "Last Name"
+        '
+        'txtGender
+        '
+        Me.txtGender.Location = New System.Drawing.Point(341, 196)
+        Me.txtGender.Name = "txtGender"
+        Me.txtGender.Size = New System.Drawing.Size(147, 20)
+        Me.txtGender.TabIndex = 5
+        '
+        'lblGender
+        '
+        Me.lblGender.AutoSize = True
+        Me.lblGender.Location = New System.Drawing.Point(238, 199)
+        Me.lblGender.Name = "lblGender"
+        Me.lblGender.Size = New System.Drawing.Size(42, 13)
+        Me.lblGender.TabIndex = 6
+        Me.lblGender.Text = "Gender"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblGender)
+        Me.Controls.Add(Me.txtGender)
+        Me.Controls.Add(Me.lblLastName)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.lblFirstName)
+        Me.Controls.Add(Me.txtFirstName)
         Me.Controls.Add(Me.btnGo)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents btnGo As Button
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents lblFirstName As Label
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents lblLastName As Label
+    Friend WithEvents txtGender As TextBox
+    Friend WithEvents lblGender As Label
 End Class
--- WindowsApp5/Form1.vb
+++ WindowsApp5/Form1.vb
@@ -1,9 +1,13 @@
 ﻿Public Class Form1
     Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
         Dim stFirstName As String
+        Dim stLastName As String
+        Dim stGender As String
 
-        stFirstName = InputBox("please enter your first name")
+        stFirstName = txtFirstName.Text   'Это значение берётся из текстового поля с названием txtFirstName
+        stLastName = txtLastName.Text
+        stGender = txtGender.Text
 
-        MsgBox("hello " & stFirstName)
+        MsgBox("hello " & stFirstName & " " & stLastName & " you are a " & stGender)
     End Sub
 End Class
