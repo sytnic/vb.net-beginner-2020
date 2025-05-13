a60fd2c49f74c687b515cdfbd288c93a1738859a
sytnic <sytnic@list.ru>
2025-05-13 10:19:24 +0300

    8.2 Complex Arithmetic Expressions

--- WindowsApp8/Form1.vb
+++ WindowsApp8/Form1.vb
@@ -1,15 +1,17 @@
 ﻿Public Class Form1
     Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
-        Dim decPrice As Decimal
-        Dim iQuantity As Integer
-        Dim decDiscount As Decimal
-        Dim decTotalCost As Decimal
+        Dim decPrice As Decimal     ' Цена
+        Dim iQuantity As Integer    ' Количество
+        Dim decDiscount As Decimal  ' Скидка на штуку
+        Dim decTotalCost As Decimal ' Общая стоимость
+        Dim decPostage As Decimal   ' Стоимость доставки
 
         decPrice = 5
         iQuantity = 10
         decDiscount = 2
+        decPostage = 3
 
-        decTotalCost = (decPrice - decDiscount) * iQuantity
+        decTotalCost = ((decPrice - decDiscount) * iQuantity) + decPostage
 
         'Вычисляется согласно порядку математических действий:
         'BODMAS, BO(DM)(AS) - Brackets Order Division Multiplication Addition Subtraction
