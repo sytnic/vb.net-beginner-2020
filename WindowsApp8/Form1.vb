dd1150b7b25f40b9646a3e104a5ba32be1f6f4c2
sytnic <sytnic@list.ru>
2025-05-13 09:57:20 +0300

    8.0 Complex Arithmetic Expressions

---
+++ WindowsApp8/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim decPrice As Decimal
+        Dim iQuantity As Integer
+        Dim decTotalCost As Decimal
+
+        decPrice = 5
+        iQuantity = 10
+
+        decTotalCost = decPrice * iQuantity
+
+        MsgBox(decTotalCost)
+
+    End Sub
+End Class
