182dbf596731e3073ea202d00ad6ea904579652b
sytnic <sytnic@list.ru>
2025-05-13 10:38:59 +0300

    8.3 Complex Arithmetic Expressions

--- WindowsApp8/Form1.vb
+++ WindowsApp8/Form1.vb
@@ -11,12 +11,34 @@
         decDiscount = 2
         decPostage = 3
 
-        decTotalCost = ((decPrice - decDiscount) * iQuantity) + decPostage
+        decTotalCost = ((decPrice - decDiscount) * iQuantity) + decPostage  '33
 
         'Вычисляется согласно порядку математических действий:
         'BODMAS, BO(DM)(AS) - Brackets Order Division Multiplication Addition Subtraction
 
+        'Please excuse my dear aunt Sally PE(MD)(AS) - Parentheses Exponentiation Multiplication Division Addition Subtraction
+
         MsgBox(decTotalCost)
+
+        Dim iResult As Integer
+        iResult = 10 - 5 + 2  '7
+        MsgBox(iResult)
+
+        iResult = 10 + 2 - 5  '7
+        MsgBox(iResult)
+
+        iResult = 10 - 2 + 5  '13
+        MsgBox(iResult)
+
+
+        iResult = 10 / 5 * 2  '4
+        MsgBox(iResult)
+
+        iResult = 2 * 10 / 5  '4
+        MsgBox(iResult)
+
+        iResult = 10 * 5 / 2  '25
+        MsgBox(iResult)
 
     End Sub
 End Class
