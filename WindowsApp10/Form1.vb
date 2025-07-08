3e19785662e26b30e170966159e2997d7dac954f
sytnic <sytnic@list.ru>
2025-07-08 09:43:51 +0300

    10.0 Logical and Relational Operators 1

---
+++ WindowsApp10/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub btnGrade_Click(sender As Object, e As EventArgs) Handles btnGrade.Click
+
+        Dim iScore As Integer
+        iScore = txtExamScore.Text
+
+        If iScore < 0 Or iScore > 100 Then
+            MsgBox("That is not a valid score. Enter a number between 0 and 100")
+        ElseIf iScore >= 50 Then
+            MsgBox("pass")
+        ElseIf iScore < 50 Then
+            MsgBox("fail")
+        End If
+
+        MsgBox("all done")
+
+    End Sub
+End Class
