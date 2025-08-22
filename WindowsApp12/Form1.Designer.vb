4ee563e5b72a458783a04c4fb3df0c1f871ecd3d
sytnic <sytnic@list.ru>
2025-08-22 11:04:55 +0300

    12. Select Case

---
+++ WindowsApp12/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtTemperature = New System.Windows.Forms.TextBox()
+        Me.btnCheck = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtTemperature
+        '
+        Me.txtTemperature.Location = New System.Drawing.Point(86, 71)
+        Me.txtTemperature.Name = "txtTemperature"
+        Me.txtTemperature.Size = New System.Drawing.Size(200, 20)
+        Me.txtTemperature.TabIndex = 0
+        '
+        'btnCheck
+        '
+        Me.btnCheck.Location = New System.Drawing.Point(88, 155)
+        Me.btnCheck.Name = "btnCheck"
+        Me.btnCheck.Size = New System.Drawing.Size(197, 65)
+        Me.btnCheck.TabIndex = 1
+        Me.btnCheck.Text = "Check Temperature"
+        Me.btnCheck.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(668, 379)
+        Me.Controls.Add(Me.btnCheck)
+        Me.Controls.Add(Me.txtTemperature)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtTemperature As TextBox
+    Friend WithEvents btnCheck As Button
+End Class
---
+++ WindowsApp12/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Sub btnCheck_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
+        Dim iTemperature As Integer
+        ' присвоение и преобразование типа в integer
+        iTemperature = CInt(txtTemperature.Text)
+
+        Dim iWindSpeed As Integer
+        iWindSpeed = 30
+
+        'If может проверить несколько переменных за раз
+
+        'If iTemperature <= 0 And iWindSpeed > 20 Then
+        '    MsgBox("That will feel really cold")
+        'End If
+
+        'Case проверяет только одну переменную за раз
+        Select Case iTemperature
+            Case Is = 0
+                MsgBox("Freezing")
+                MsgBox("Water will freeze")
+                MsgBox("and you can go skating")
+            Case Is < 0
+                MsgBox("Sub zero")
+                'If можно вкладывать в конструкцию Case
+                If iWindSpeed > 20 Then
+                    MsgBox("That will feel really cold")
+                End If
+            Case 1 To 10    '1 и 10 включительно
+                MsgBox("Cold")
+            Case 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+                MsgBox("Warm")
+            Case Else
+                MsgBox("Hot")
+        End Select
+
+    End Sub
+End Class
