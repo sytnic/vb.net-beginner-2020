2b19c330fde4b5fc9a6b6a4da130f3898bb1cf72
sytnic <dm.sytnic@yandex.ru>
2025-09-26 10:01:36 +0300

    14. Practice For Next Loops & If Blocks

---
+++ WindowsApp14/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub btnCountUpTask_Click(sender As Object, e As EventArgs) Handles btnCountUpTask.Click
+
+        ' максимальное число счёта
+        Dim iMax As Integer
+        ' нечетные или четные числа считать
+        Dim stoddorEven As String
+        ' счётчик в циклах
+        Dim x As Integer
+
+
+        iMax = InputBox("What number do you want to count up to?")
+        stoddorEven = InputBox("Do you want odd numbers or even numbers?")
+
+        If stoddorEven = "even" Then
+            ' если четное, то считать от 2 по 2
+            For x = 2 To iMax Step 2
+                MsgBox(x)
+            Next
+        ElseIf stoddorEven = "odd" Then
+            ' если нечетное, то считать от 1 по 2
+            For x = 1 To iMax Step 2
+                MsgBox(x)
+            Next
+        End If
+
+    End Sub
+End Class
---
+++ WindowsApp14/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WindowsApp14")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WindowsApp14")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d52b9f18-5ff3-4616-8baa-88d1a168f4d2")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
