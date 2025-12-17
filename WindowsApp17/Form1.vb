297c323fbea169d265a73cb38e0c984fb20ddcb5
sytnic <dm.sytnic@yandex.ru>
2025-12-17 09:21:35 +0300

    17. Array Variables

---
+++ WindowsApp17/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        ' Объявление массива
+        Dim astFruits(4) As String
+
+        astFruits(0) = "Banana"
+        astFruits(1) = "Orange"
+        astFruits(2) = "Pineapple"
+        astFruits(3) = "Strawberry"
+        astFruits(4) = "Mango"
+
+        Dim i As Integer
+
+        ' Вывод всех элементов через цикл
+        For i = 0 To 4
+            MsgBox(astFruits(i))
+        Next
+
+    End Sub
+End Class
---
+++ WindowsApp17/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WindowsApp17")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WindowsApp17")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5ea03d51-b309-4679-bb41-095334e75882")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
