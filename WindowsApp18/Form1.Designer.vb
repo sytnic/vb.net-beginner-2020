3be06d0d7ad91f5cf3e580ef2cd4db2ba53f3e60
sytnic <dm.sytnic@yandex.ru>
2026-01-30 09:42:46 +0300

    18. Practice Arrays & Loops

---
+++ WindowsApp18/Form1.Designer.vb
@@ -0,0 +1,292 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label0 = New System.Windows.Forms.Label()
+        Me.btnExercise1 = New System.Windows.Forms.Button()
+        Me.btnExercise2 = New System.Windows.Forms.Button()
+        Me.btnExercise3 = New System.Windows.Forms.Button()
+        Me.btnExercise4 = New System.Windows.Forms.Button()
+        Me.btnExercise5 = New System.Windows.Forms.Button()
+        Me.btnExercise6 = New System.Windows.Forms.Button()
+        Me.btnExercise7 = New System.Windows.Forms.Button()
+        Me.btnExercise8 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label0
+        '
+        Me.Label0.AutoSize = True
+        Me.Label0.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label0.Location = New System.Drawing.Point(52, 18)
+        Me.Label0.Name = "Label0"
+        Me.Label0.Size = New System.Drawing.Size(332, 20)
+        Me.Label0.TabIndex = 0
+        Me.Label0.Text = "Coding exercises using arrays with loops"
+        '
+        'btnExercise1
+        '
+        Me.btnExercise1.Location = New System.Drawing.Point(56, 58)
+        Me.btnExercise1.Name = "btnExercise1"
+        Me.btnExercise1.Size = New System.Drawing.Size(104, 35)
+        Me.btnExercise1.TabIndex = 1
+        Me.btnExercise1.Text = "Exercise1"
+        Me.btnExercise1.UseVisualStyleBackColor = True
+        '
+        'btnExercise2
+        '
+        Me.btnExercise2.Location = New System.Drawing.Point(56, 108)
+        Me.btnExercise2.Name = "btnExercise2"
+        Me.btnExercise2.Size = New System.Drawing.Size(104, 35)
+        Me.btnExercise2.TabIndex = 2
+        Me.btnExercise2.Text = "Exercise2"
+        Me.btnExercise2.UseVisualStyleBackColor = True
+        '
+        'btnExercise3
+        '
+        Me.btnExercise3.Location = New System.Drawing.Point(56, 158)
+        Me.btnExercise3.Name = "btnExercise3"
+        Me.btnExercise3.Size = New System.Drawing.Size(104, 35)
+        Me.btnExercise3.TabIndex = 4
+        Me.btnExercise3.Text = "Exercise3"
+        Me.btnExercise3.UseVisualStyleBackColor = True
+        '
+        'btnExercise4
+        '
+        Me.btnExercise4.Location = New System.Drawing.Point(56, 211)
+        Me.btnExercise4.Name = "btnExercise4"
+        Me.btnExercise4.Size = New System.Drawing.Size(104, 35)
+        Me.btnExercise4.TabIndex = 3
+        Me.btnExercise4.Text = "Exercise4"
+        Me.btnExercise4.UseVisualStyleBackColor = True
+        '
+        'btnExercise5
+        '
+        Me.btnExercise5.Location = New System.Drawing.Point(56, 261)
+        Me.btnExercise5.Name = "btnExercise5"
+        Me.btnExercise5.Size = New System.Drawing.Size(104, 35)
+        Me.btnExercise5.TabIndex = 8
+        Me.btnExercise5.Text = "Exercise5"
+        Me.btnExercise5.UseVisualStyleBackColor = True
+        '
+        'btnExercise6
+        '
+        Me.btnExercise6.Location = New System.Drawing.Point(56, 311)
+        Me.btnExercise6.Name = "btnExercise6"
+        Me.btnExercise6.Size = New System.Drawing.Size(104, 35)
+        Me.btnExercise6.TabIndex = 7
+        Me.btnExercise6.Text = "Exercise6"
+        Me.btnExercise6.UseVisualStyleBackColor = True
+        '
+        'btnExercise7
+        '
+        Me.btnExercise7.Location = New System.Drawing.Point(56, 362)
+        Me.btnExercise7.Name = "btnExercise7"
+        Me.btnExercise7.Size = New System.Drawing.Size(104, 35)
+        Me.btnExercise7.TabIndex = 6
+        Me.btnExercise7.Text = "Exercise7"
+        Me.btnExercise7.UseVisualStyleBackColor = True
+        '
+        'btnExercise8
+        '
+        Me.btnExercise8.Location = New System.Drawing.Point(56, 417)
+        Me.btnExercise8.Name = "btnExercise8"
+        Me.btnExercise8.Size = New System.Drawing.Size(104, 35)
+        Me.btnExercise8.TabIndex = 5
+        Me.btnExercise8.Text = "Exercise8"
+        Me.btnExercise8.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label1.Location = New System.Drawing.Point(183, 67)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(375, 16)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Output each item in a separate message box one after another"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label2.Location = New System.Drawing.Point(183, 119)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(394, 16)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Output all of the items in the same message box on separate lines"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label3.Location = New System.Drawing.Point(187, 169)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(362, 16)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Add up all of the items then output the total in a message box"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label4.Location = New System.Drawing.Point(187, 222)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(393, 16)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Calculate the average of the items then output it in a message box"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label5.Location = New System.Drawing.Point(187, 274)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(429, 16)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Add up the items greater than 20 then output the result in a message box"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label6.Location = New System.Drawing.Point(187, 325)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(311, 16)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "Find the largest item then output it in a message box"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label7.Location = New System.Drawing.Point(189, 377)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(320, 16)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "Find the smallest item then output it in a message box"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label8.Location = New System.Drawing.Point(189, 428)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(555, 16)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = "Replace each item in the array with a new value that is twice as big then output " &
+    "the new values"
+        '
+        'Label9
+        '
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label9.Location = New System.Drawing.Point(690, 41)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(258, 42)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "Begin each procedure with the following code to declare an initialise an array of" &
+    " integers"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label10.Location = New System.Drawing.Point(690, 93)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(141, 15)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "Dim aiData(4) As Integer"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label11.Location = New System.Drawing.Point(690, 128)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(85, 75)
+        Me.Label11.TabIndex = 19
+        Me.Label11.Text = "aiData(0) = 5" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "aiData(1) = 7" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "aiData(2) = 33" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "aiData(3) = 22" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "aiData(4) = 8"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(970, 492)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExercise5)
+        Me.Controls.Add(Me.btnExercise6)
+        Me.Controls.Add(Me.btnExercise7)
+        Me.Controls.Add(Me.btnExercise8)
+        Me.Controls.Add(Me.btnExercise3)
+        Me.Controls.Add(Me.btnExercise4)
+        Me.Controls.Add(Me.btnExercise2)
+        Me.Controls.Add(Me.btnExercise1)
+        Me.Controls.Add(Me.Label0)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label0 As Label
+    Friend WithEvents btnExercise1 As Button
+    Friend WithEvents btnExercise2 As Button
+    Friend WithEvents btnExercise3 As Button
+    Friend WithEvents btnExercise4 As Button
+    Friend WithEvents btnExercise5 As Button
+    Friend WithEvents btnExercise6 As Button
+    Friend WithEvents btnExercise7 As Button
+    Friend WithEvents btnExercise8 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+End Class
---
+++ WindowsApp18/Form1.vb
@@ -0,0 +1,219 @@
+﻿Public Class Form1
+    Private Sub btnExercise1_Click(sender As Object, e As EventArgs) Handles btnExercise1.Click
+
+        ' Output each item in a separate message box one after another
+        ' Вывод всех чисел по очереди
+
+        ' ai - a массив, i числовой
+        Dim aiData(4) As Integer
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer
+
+        For i = 0 To 4
+            MsgBox(aiData(i))
+        Next
+
+    End Sub
+
+    Private Sub btnExercise2_Click(sender As Object, e As EventArgs) Handles btnExercise2.Click
+
+        ' Output all of the items in the same message box on separate lines
+        ' Вывод всех чисел за раз
+
+        Dim aiData(4) As Integer
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer
+        Dim stOut As String
+
+        For i = 0 To 4
+            ' Здесь в итерациях происходит конкатенация предыдущей строки с новой,
+            ' своего рода output=.output (php).
+            stOut = stOut & aiData(i) & vbNewLine
+        Next
+
+        ' Вывести весь результат за раз
+        MsgBox(stOut)
+
+    End Sub
+
+    Private Sub btnExercise3_Click(sender As Object, e As EventArgs) Handles btnExercise3.Click
+
+        ' Add up all of the items then output the total in a message box
+        ' Сумма всех чисел
+
+        Dim aiData(4) As Integer
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer
+        Dim iTotal As Integer
+
+        For i = 0 To 4
+            iTotal = iTotal + aiData(i)
+        Next
+
+        MsgBox("The total is " & iTotal)
+
+    End Sub
+
+    Private Sub btnExercise4_Click(sender As Object, e As EventArgs) Handles btnExercise4.Click
+
+        ' Calculate the average of the items then output it in a message box
+        ' Среднее всех чисел
+
+        Dim aiData(4) As Integer
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer
+        Dim iTotal As Integer
+
+        For i = 0 To 4
+            iTotal = iTotal + aiData(i)
+        Next
+
+        MsgBox("The average is " & iTotal / 5)
+
+    End Sub
+
+    Private Sub btnExercise5_Click(sender As Object, e As EventArgs) Handles btnExercise5.Click
+
+        ' Add up the items greater than 20 then output the result in a message box
+        ' Сложить все элементы больше 20
+
+        Dim aiData(4) As Integer
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer
+        Dim iTotal As Integer
+
+        For i = 0 To 4
+            If aiData(i) > 20 Then
+                iTotal = iTotal + aiData(i)
+            End If
+        Next
+
+        MsgBox("The total of the items bigger than 20 is " & iTotal)
+
+    End Sub
+
+    Private Sub btnExercise6_Click(sender As Object, e As EventArgs) Handles btnExercise6.Click
+
+        ' Find the largest item then output it in a message box
+        ' Найти наибольшее значение в массиве
+
+        Dim aiData(4) As Integer
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer
+        Dim iMax As Integer
+
+        iMax = 0
+
+        ' На каждой итерации сравниваем значение iMax с текущим из массива,
+        ' и если из массива больше,
+        ' то присваиваем его к iMax
+        For i = 0 To 4
+            If aiData(i) > iMax Then
+                iMax = aiData(i)
+            End If
+        Next
+
+        MsgBox("The largest item is " & iMax)
+
+    End Sub
+
+    Private Sub btnExercise7_Click(sender As Object, e As EventArgs) Handles btnExercise7.Click
+
+        ' Find the smallest item then output it in a message box
+        ' Найти наименьшее значение в массиве
+
+        Dim aiData(4) As Integer
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer
+        Dim iMin As Integer
+
+        iMin = 10000
+
+        ' На каждой итерации сравниваем значение iMin с текущим из массива,
+        ' и если из массива меньше,
+        ' то присваиваем его к iMin
+        For i = 0 To 4
+            If aiData(i) < iMin Then
+                iMin = aiData(i)
+            End If
+        Next
+
+        MsgBox("The smallest item is " & iMin)
+
+    End Sub
+
+    Private Sub btnExercise8_Click(sender As Object, e As EventArgs) Handles btnExercise8.Click
+
+        ' Replace each item in the array with a new value that is twice as big then output the new values
+        ' Вывести за раз все числа массива, каждое из которых помножено на 2
+
+        Dim aiData(4) As Integer
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer
+
+        ' Удваиваем все значения массива
+        For i = 0 To 4
+            aiData(i) = aiData(i) * 2
+        Next
+
+
+        Dim stOut As String
+
+        ' Выводим все значения за раз
+        For i = 0 To 4
+            stOut = stOut & aiData(i) & vbNewLine
+        Next
+
+        MsgBox(stOut)
+
+    End Sub
+
+End Class
