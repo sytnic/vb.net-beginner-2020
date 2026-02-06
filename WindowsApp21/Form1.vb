14a4110799b398687e9bdabd51eb54b4d30a34f0
sytnic <dm.sytnic@yandex.ru>
2026-02-06 10:16:48 +0300

    21. 2D Arrays & Nested Loops

---
+++ WindowsApp21/Form1.vb
@@ -0,0 +1,246 @@
+﻿Public Class Form1
+    Private Sub btnRowWise_Click(sender As Object, e As EventArgs) Handles btnRowWise.Click
+
+        Dim astPeople(4, 5) As String
+
+        astPeople(0, 0) = "Barack"
+        astPeople(1, 0) = "Obama"
+        astPeople(2, 0) = "Male"
+        astPeople(3, 0) = "American"
+        astPeople(4, 0) = "President"
+
+        astPeople(0, 1) = "Jacinda"
+        astPeople(1, 1) = "Ardern"
+        astPeople(2, 1) = "Female"
+        astPeople(3, 1) = "New Zealand"
+        astPeople(4, 1) = "Prime Minister"
+
+        astPeople(0, 2) = "Ada"
+        astPeople(1, 2) = "Lovelace"
+        astPeople(2, 2) = "Female"
+        astPeople(3, 2) = "British"
+        astPeople(4, 2) = "Mathematician"
+
+        astPeople(0, 3) = "Albert"
+        astPeople(1, 3) = "Einstein"
+        astPeople(2, 3) = "Male"
+        astPeople(3, 3) = "Swiss"
+        astPeople(4, 3) = "Scientist"
+
+        astPeople(0, 4) = "Mahatma"
+        astPeople(1, 4) = "Gandhi"
+        astPeople(2, 4) = "Male"
+        astPeople(3, 4) = "Indian"
+        astPeople(4, 4) = "Lawyer"
+
+        astPeople(0, 5) = "Vincent"
+        astPeople(1, 5) = "van Gogh"
+        astPeople(2, 5) = "Male"
+        astPeople(3, 5) = "Dutch"
+        astPeople(4, 5) = "Artist"
+
+        Dim x As Integer
+        Dim y As Integer
+
+        ' Для каждой строки
+        For y = 0 To 5
+            ' перебрать каждый столбец
+            For x = 0 To 4
+                MsgBox(astPeople(x, y))
+            Next
+        Next
+
+    End Sub
+
+    Private Sub btnColumnWise_Click(sender As Object, e As EventArgs) Handles btnColumnWise.Click
+
+        Dim astPeople(4, 5) As String
+
+        astPeople(0, 0) = "Barack"
+        astPeople(1, 0) = "Obama"
+        astPeople(2, 0) = "Male"
+        astPeople(3, 0) = "American"
+        astPeople(4, 0) = "President"
+
+        astPeople(0, 1) = "Jacinda"
+        astPeople(1, 1) = "Ardern"
+        astPeople(2, 1) = "Female"
+        astPeople(3, 1) = "New Zealand"
+        astPeople(4, 1) = "Prime Minister"
+
+        astPeople(0, 2) = "Ada"
+        astPeople(1, 2) = "Lovelace"
+        astPeople(2, 2) = "Female"
+        astPeople(3, 2) = "British"
+        astPeople(4, 2) = "Mathematician"
+
+        astPeople(0, 3) = "Albert"
+        astPeople(1, 3) = "Einstein"
+        astPeople(2, 3) = "Male"
+        astPeople(3, 3) = "Swiss"
+        astPeople(4, 3) = "Scientist"
+
+        astPeople(0, 4) = "Mahatma"
+        astPeople(1, 4) = "Gandhi"
+        astPeople(2, 4) = "Male"
+        astPeople(3, 4) = "Indian"
+        astPeople(4, 4) = "Lawyer"
+
+        astPeople(0, 5) = "Vincent"
+        astPeople(1, 5) = "van Gogh"
+        astPeople(2, 5) = "Male"
+        astPeople(3, 5) = "Dutch"
+        astPeople(4, 5) = "Artist"
+
+        Dim x As Integer
+        Dim y As Integer
+
+        ' Для каждого столбца
+        For x = 0 To 4
+            ' перебрать каждую строку
+            For y = 0 To 5
+                MsgBox(astPeople(x, y))
+            Next
+        Next
+
+    End Sub
+
+    Private Sub btnOutputAll_Click(sender As Object, e As EventArgs) Handles btnOutputAll.Click
+
+        Dim astPeople(4, 5) As String
+
+        astPeople(0, 0) = "Barack"
+        astPeople(1, 0) = "Obama"
+        astPeople(2, 0) = "Male"
+        astPeople(3, 0) = "American"
+        astPeople(4, 0) = "President"
+
+        astPeople(0, 1) = "Jacinda"
+        astPeople(1, 1) = "Ardern"
+        astPeople(2, 1) = "Female"
+        astPeople(3, 1) = "New Zealand"
+        astPeople(4, 1) = "Prime Minister"
+
+        astPeople(0, 2) = "Ada"
+        astPeople(1, 2) = "Lovelace"
+        astPeople(2, 2) = "Female"
+        astPeople(3, 2) = "British"
+        astPeople(4, 2) = "Mathematician"
+
+        astPeople(0, 3) = "Albert"
+        astPeople(1, 3) = "Einstein"
+        astPeople(2, 3) = "Male"
+        astPeople(3, 3) = "Swiss"
+        astPeople(4, 3) = "Scientist"
+
+        astPeople(0, 4) = "Mahatma"
+        astPeople(1, 4) = "Gandhi"
+        astPeople(2, 4) = "Male"
+        astPeople(3, 4) = "Indian"
+        astPeople(4, 4) = "Lawyer"
+
+        astPeople(0, 5) = "Vincent"
+        astPeople(1, 5) = "van Gogh"
+        astPeople(2, 5) = "Male"
+        astPeople(3, 5) = "Dutch"
+        astPeople(4, 5) = "Artist"
+
+        Dim x As Integer
+        Dim y As Integer
+        Dim stOut As String
+
+        ' Для каждой строки
+        For y = 0 To 5
+            ' перебрать каждый столбец
+            For x = 0 To 4
+                ' Добавлять всё в одну строку
+                stOut = stOut & astPeople(x, y) & " "
+            Next
+            stOut = stOut & vbNewLine
+        Next
+
+        ' Вывести сообщение по клику на кнопке
+        MsgBox(stOut)
+
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+
+        Dim astPeople(4, 5) As String
+
+        astPeople(0, 0) = "Barack"
+        astPeople(1, 0) = "Obama"
+        astPeople(2, 0) = "Male"
+        astPeople(3, 0) = "American"
+        astPeople(4, 0) = "President"
+
+        astPeople(0, 1) = "Jacinda"
+        astPeople(1, 1) = "Ardern"
+        astPeople(2, 1) = "Female"
+        astPeople(3, 1) = "New Zealand"
+        astPeople(4, 1) = "Prime Minister"
+
+        astPeople(0, 2) = "Ada"
+        astPeople(1, 2) = "Lovelace"
+        astPeople(2, 2) = "Female"
+        astPeople(3, 2) = "British"
+        astPeople(4, 2) = "Mathematician"
+
+        astPeople(0, 3) = "Albert"
+        astPeople(1, 3) = "Einstein"
+        astPeople(2, 3) = "Male"
+        astPeople(3, 3) = "Swiss"
+        astPeople(4, 3) = "Scientist"
+
+        astPeople(0, 4) = "Mahatma"
+        astPeople(1, 4) = "Gandhi"
+        astPeople(2, 4) = "Male"
+        astPeople(3, 4) = "Indian"
+        astPeople(4, 4) = "Lawyer"
+
+        astPeople(0, 5) = "Vincent"
+        astPeople(1, 5) = "van Gogh"
+        astPeople(2, 5) = "Male"
+        astPeople(3, 5) = "Dutch"
+        astPeople(4, 5) = "Artist"
+
+        Dim x As Integer
+        Dim y As Integer
+        Dim bFound As Boolean
+        Dim stTarget As String
+        Dim stOut As String
+
+        bFound = False
+
+        ' Вывести окно для ввода текста
+        stTarget = InputBox("Enter the surname of the person you want to find")
+
+        ' Для каждой строки
+        For y = 0 To 5
+            ' если значение столбца 1 (фамилии) совпало с введённым текстом, то
+            If astPeople(1, y) = stTarget Then
+                ' задать переменной True и полностью выйти из цикла
+                bFound = True
+                Exit For
+            End If
+        Next
+
+        ' Если bFound True, тогда
+        If bFound = True Then
+            ' для каждого значения столбца
+            For x = 0 To 4
+                ' добавлять в одну строку все значения из сохранённой строки,
+                ' сохранённая строка - это значение y,
+                ' оно было присвоено в предыдущем цикле при выходе из него и остаётся таковым
+                stOut = stOut & astPeople(x, y) & " "
+            Next
+        Else
+            ' Иначе сохранить строку, что ничего не найдено
+            stOut = "not found"
+        End If
+
+        ' Вывести получившуюся строку
+        MsgBox(stOut)
+
+    End Sub
+End Class
---
+++ WindowsApp21/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WindowsApp21")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WindowsApp21")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("79e97fe5-8183-434d-a135-17c6659d667a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
